ac9d425311083dec1d22ac56d8ca4c5da397621b
kaingi <72867187+kaingi@users.noreply.github.com>
2025-07-09 16:06:34 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,4 @@
--- Initial query (before optimization)
+-- Initial query with WHERE and AND for performance analysis
 EXPLAIN ANALYZE
 SELECT 
     b.booking_id,
@@ -6,35 +6,24 @@ SELECT
     b.end_date,
     b.total_price,
     b.status,
-    u.user_id,
+
     u.first_name,
     u.last_name,
     u.email,
-    p.property_id,
+
     p.name AS property_name,
     p.location,
     p.pricepernight,
-    pay.payment_id,
+
     pay.amount,
     pay.payment_method,
     pay.payment_date
+
 FROM Booking b
 JOIN Users u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
 
--- Refactored version (optimized)
-EXPLAIN ANALYZE
-SELECT 
-    b.booking_id,
-    b.start_date,
-    b.status,
-    u.first_name,
-    u.email,
-    p.name AS property_name,
-    pay.amount
-FROM Booking b
-JOIN Users u ON b.user_id = u.user_id
-JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
-
+-- ✅ Filtering logic
+WHERE b.status = 'confirmed'
+  AND p.location = 'Nairobi, Kenya';
